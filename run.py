90df0259bfd7e17fc4e368faacb3ccec803efeff
EricOuma <odhiamboeric0049@gmail.com>
2018-12-10 22:47:16 +0300

    API completed

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask_httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
 
 app = Flask(__name__)
 
@@ -17,6 +19,16 @@ tasks = [
     }
 ]
 
+@auth.get_password
+def get_password(username):
+    if username == 'eric':
+        return 'odhiambo'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
 @app.route('/')
 @app.route('/todo')
 def home():
@@ -24,10 +36,75 @@ def home():
 
 
 @app.route('/todo/tasks', methods=['GET'])
+@auth.login_required
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+
+@app.route('/todo/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task  = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return make_response(jsonify({'task': task[0]}))
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return jsonify({'error': 'Not found'}), 404
+
+
+@app.route('/todo/tasks/add', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+
+@app.route('/todo/tasks/update/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+
+@app.route('/todo/tasks/delete/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
